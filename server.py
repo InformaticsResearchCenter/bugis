541e4cfe6dbc90bb4f9ebad01ef417f875d9a30e
Rolly Maulana Awangga <rolly@awang.ga>
2022-11-06 11:13:11 +0700

    first

---
+++ server.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from blacksheep import Application
+
+
+app = Application()
+
+@app.route("/")
+async def home():
+    return f"Hello, World! {datetime.utcnow().isoformat()}"
