5b7b87650dfbc8964af395f91d9f32cfefce7643
Rolly Maulana Awangga <rolly@awang.ga>
2022-11-07 23:52:29 +0700

    test with kambing hitam

--- test_module.py
+++ test_module.py
@@ -1,13 +1,9 @@
 from module.gis.SG import seletar
 from model import data
 
-def inc(x):
-    return x + 1
-
-
-def test_answer():
+def test_seletar_airport():
     gis=data.Gis(103.87068815153685,1.4155690028533765,"seletar airport")
-    resp ='{
-  "message": "shp file seletar airport was created"
-}'
-    assert seletar.airport(gis) == resp
+    assert seletar.airport(gis).status == 200
+
+def test_seletar_getairport():
+    assert seletar.getairport(0).status == 200
