29036fa044063d10e9383dfd73ff5cbf9867a0d7
Rolly Maulana Awangga <rolly@awang.ga>
2022-11-07 23:37:22 +0700

    deserialize param post

--- module/gis/SG/seletar.py
+++ module/gis/SG/seletar.py
@@ -4,10 +4,10 @@ import shapefile
 def airport(gis):
     w = shapefile.Writer('static/shp/seletar/airport')
     w.field('name', 'C')
-    w.point(gis.value.longitude, gis.value.latitude) 
-    w.record(gis.value.name)
+    w.point(gis.longitude, gis.latitude) 
+    w.record(gis.name)
     w.close()
-    msg='shp file '+gis.value.name+' was created'
+    msg='shp file '+gis.name+' was created'
     return json({"message" : msg})
 
 def getairport(param):
