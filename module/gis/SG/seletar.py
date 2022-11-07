2b2fab970bdd3cb146a09104e34ee45ee5f920b5
Rolly Maulana Awangga <rolly@awang.ga>
2022-11-07 19:02:47 +0700

    set seletar save point

--- module/gis/SG/seletar.py
+++ module/gis/SG/seletar.py
@@ -1,2 +1,9 @@
+from blacksheep import json
+
 def airport(gis):
-    return gis.value
+    w = shapefile.Writer('static/shp/seletar/airport')
+    w.field('name', 'C')
+    w.point(gis.value.longitude, gis.value.latitude) 
+    w.record(gis.value.name)
+    w.close()
+    return json({"message" : "shp file {gis.value.name} created"})
